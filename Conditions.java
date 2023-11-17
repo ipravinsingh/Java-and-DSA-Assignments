c7c68d52c74b2ea1479bc507ce4075ec624fa087
Praveen Singh <singhpravu@gmail.com>
2023-11-17 21:53:15 +0530

    Day-1

---
+++ Conditions.java
@@ -0,0 +1,71 @@
+// // Question_1
+// import java.util.*;
+// public class Conditions {
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+// int number = sc.nextInt();
+// if (number > 0) {
+// System.out.println("Positive number");
+// } else {
+// System.out.println("Negative number");
+// }
+// }
+// }
+
+// // Question_2
+// public class Conditions {
+// public static void main(String[] args) {
+// double temp = 10;
+// if (temp > 100) {
+// System.out.println("You have fever");
+// } else {
+// System.out.println("You don't have fever");
+// }
+// }
+// }
+
+// // Question_3
+// import java.util.*;
+// public class Conditions {
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+// int week = sc.nextInt();
+// switch (week) {
+// case 1:
+// System.out.println("Sunday");
+// break;
+// case 2:
+// System.out.println("Monday");
+// break;
+// case 3:
+// System.out.println("Tuesday");
+// break;
+// case 4:
+// System.out.println("Wednesday");
+// break;
+// case 5:
+// System.out.println("Thursday");
+// break;
+// case 6:
+// System.out.println("Friday");
+// break;
+// case 7:
+// System.out.println("Saturday");
+// break;
+// default:
+// System.out.println("Not a valid day");
+// break;
+// }
+// }
+// }
+
+// // Question_4
+// public class Conditions {
+// public static void main(String args[]) {
+// int a = 63, b = 36;
+// boolean x = (a < b) ? true : false;
+// int y = (a > b) ? a : b;
+// System.out.println(x);
+// System.out.println(y);
+// }
+// }
---
+++ Variables.java
@@ -0,0 +1,39 @@
+// // Question_1
+// import java.util.*;
+// public class Variables {
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+// int A = sc.nextInt();
+// int B = sc.nextInt();
+// int C = sc.nextInt();
+// float Avg = (A + B + C) / 3;
+// System.out.println(Avg);
+// }
+// }
+
+// // Question_2
+// import java.util.Scanner;
+// public class Variables {
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+// int side = sc.nextInt();
+// float area = side * side;
+// System.out.println(area);
+// }
+// }
+
+// // Question_3
+// import java.util.*;
+// public class Variables {
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+// float pencil = sc.nextFloat();
+// float pen = sc.nextFloat();
+// float eraser = sc.nextFloat();
+// float totalCost = (pencil + pen + eraser);
+// float totalCostWithGst = (pencil + pen + eraser) + (pencil + pen + eraser) *
+// 18 / 100;
+// System.out.println(totalCost);
+// System.out.println(totalCostWithGst);
+// }
+// }
