9268969836e19b4b4cfa8d9418da5ecd7c6d1168
Praveen Singh <singhpravu@gmail.com>
2023-12-06 18:36:00 +0530

    Array

--- Array.java
+++ Array.java
@@ -1,5 +1,17 @@
-public class Array {
-    public static void main(String[] args) {
-
-    }
-}
+// Question_1
+// public class Array {
+// public static boolean returnTrue(int nums[]) {
+// for (int i = 0; i < nums.length - 1; i++) {
+// for (int j = i + 1; j < nums.length; j++) {
+// if (nums[i] == nums[j]) {
+// return true;
+// }
+// }
+// }
+// return false;
+// }
+// public static void main(String[] args) {
+// int nums[] = { 1, 2, 3 };
+// System.out.println(returnTrue(nums));
+// }
+// }
